8b87def9e05727e53a34e9204cdc2d112b3ee2f6
Darkq <113276529+xDarknessXD@users.noreply.github.com>
2025-05-24 03:07:45 +0800

    edit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ export default function TabLayout() {
         tabBarActiveTintColor: COLORS.primary,
         tabBarInactiveTintColor: COLORS.grey,
         tabBarStyle: {
-            backgroundColor: "grey",
+            backgroundColor: "black",
             borderTopWidth: 0,
             position: "absolute",
             elevation: 0,
